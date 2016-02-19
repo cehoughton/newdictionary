f6911d3739b13ca6ab73931a9b4e1763ee9d951e
chris <cehoughton71@gmail.com>
2016-02-19 10:00:15 -0800

    Definitions will pass all tests

--- src/test/java/DefinitionsTest.java
+++ src/test/java/DefinitionsTest.java
@@ -19,13 +19,36 @@ public class DefinitionsTest {
   }
 
   @Test
-  public void all_returnsAllInstancesOfDefinition_true() {
+  public void all_returnsAllInstancesOfDefinitions_true() {
     Definitions firstDefinition = new Definitions("favorite toy");
     Definitions secondDefinition = new Definitions("best toy");
     assertTrue(Definitions.all().contains(firstDefinition));
     assertTrue(Definitions.all().contains(secondDefinition));
+  }
 
+  @Test
+  public void newId_DefinitionsInstantiateWithAnID_true() {
+    Definitions myDefinition = new Definitions("favorite toy");
+    assertEquals(Definitions.all().size(), myDefinition.getId());
+  }
 
+  @Test
+  public void find_returnsDefinitionsWithSameId_secondDefinitions() {
+    Definitions firstDefinition = new Definitions("favorite toy");
+    Definitions secondDefinition = new Definitions("best toy");
+    assertEquals(Definitions.find(secondDefinition.getId()), secondDefinition);
+  }
+
+  @Test
+  public void find_returnsNullWhenNoDefinitionsFound_null() {
+    assertTrue(Definitions.find(999) == null);
+  }
+
+  @Test
+  public void clear_emptiesAllDefinitionssFromArrayList() {
+    Definitions myDefinition = new Definitions("A tasty snack");
+    Definitions.clear();
+    assertEquals(Definitions.all().size(), 0);
   }
 
 
